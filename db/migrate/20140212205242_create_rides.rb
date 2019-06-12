a1b3defd42d0b347d30aa9e44715b028caf3b521
Tess Neau <tessneau@gmail.com>
2019-06-11 22:28:08 -0400

    done

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.references :rides, foreign_key: true
+      t.references :taxis, foreign_key: true
       t.timestamps null: false
     end
   end
